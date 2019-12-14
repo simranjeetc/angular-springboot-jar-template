8251d43fc9b4dff64e362988ac731e4619969744
Simranjeet Singh Chawla <Simranjeet.SinghChawla@microfocus.com>
2019-12-14 16:26:21 +0530

    Initial commit

--- opengrok-config-service/src/main/java/com/opengrok/Controller.java
+++ opengrok-config-service/src/main/java/com/opengrok/Controller.java
@@ -1,3 +1,5 @@
+package com.opengrok;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- opengrok-config-service/src/main/java/com/opengrok/OpenGrokConfigApplication.java
+++ opengrok-config-service/src/main/java/com/opengrok/OpenGrokConfigApplication.java
@@ -1,3 +1,5 @@
+package com.opengrok;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
